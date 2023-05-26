575b0a7721dacae357ae80a89c1f4d82992f805b
Simransingh010 <simran.ahuja0010@gmail.com>
2023-05-26 06:58:25 +0530

    commit 2

--- first.cpp
+++ first.cpp
@@ -3,14 +3,15 @@ using namespace std;
 
 
 int main(){
-int n;
-cin>>n;
-if (n>0){
-  cout<<"A is a positive Integer"<< endl;  
-}
-else {
-    cout<<"A is a negative Integer"<< endl;
-}
-
+ int n;
+ cin>>n;
+ if (n>0)
+ {
+  cout<<"the number is positive"<<endl;
+ }
+ else if (n<0)
+ {
+  cout<<"the number is negative"<<endl;
+ }
     return 0;
 }
