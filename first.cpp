9e37d497b9cab8de60dc9af43fc67a5f03cd08b4
Simransingh010 <simran.ahuja0010@gmail.com>
2023-05-28 10:37:02 +0530

    3rd

--- first.cpp
+++ first.cpp
@@ -1,13 +1,23 @@
-#include<iostream>
+#include <iostream>
+#include <conio.h>
+
 using namespace std;
 
+int main()
+{
+  
+  
+  int sum = 0;
+  int i=1;
+  int n;
+  cout<<"Enter your word: ";
+  cin>>n;
 
-int main(){
- int a,b;
- cin>>a>>b;
-
- cout<<"the value of A and B is "<<a<<" and "<<b<<endl;
-
- 
+  while(i<=n){
+    sum=sum+i;
+   
+    i=i+1;
+  }
+ cout<<sum;
     return 0;
 }
